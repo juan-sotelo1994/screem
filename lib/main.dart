611f9a4e3c4a779500c1ec4ef7c5498faa5f72f1
Juan David Sotelo <131626247+juan-sotelo1994@users.noreply.github.com>
2025-07-28 19:25:05 -0500

    ver2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:screem/presen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,44 +8,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      debugShowCheckedModeBanner: false,
-      home: AnimatedSplashScreen(
-        splash: 'assets/lego.gif',
-        splashIconSize: 2000.0,
-        centered: true,
-        nextScreen: const HomeScreen(),
-        backgroundColor: Colors.white,
-        duration: 3100,
-      ),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/lego.png', width: 80.0),
-            const Text(
-              'Welcome to the LEGO App',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 55.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return prese();
   }
 }
